ad857d8bd4adaa1ef247e45690c424a112500332
Vinicius Tibo <viniciustibo.vt@gmail.com>
2024-07-13 11:20:11 -0300

    add sound for hitting the paddle

--- components/ball.lua
+++ components/ball.lua
@@ -1,5 +1,6 @@
 local ball = {}
 
+local ball_hit_paddle_sound = love.audio.newSource('assets/sounds/ball-hitting-paddle.mp3', 'static')
 ball.x = love.graphics.getWidth() / 2
 ball.y = love.graphics.getHeight() / 2
 ball.size = 10
@@ -10,11 +11,13 @@ local function paddles_collision(paddle1, paddle2)
   if ball.x < paddle1.x + 20 + ball.size and ball.y + ball.size >= paddle1.y and ball.y <= paddle1.y + 100 then
     ball.x = paddle1.x + 20 + ball.size
     ball.speed_x = -ball.speed_x
+    ball_hit_paddle_sound:play()
   end
 
   if ball.x > paddle2.x - ball.size and ball.y + ball.size >= paddle2.y and ball.y <= paddle2.y + 100 then
     ball.x = paddle2.x - ball.size
     ball.speed_x = -ball.speed_x
+    ball_hit_paddle_sound:play()
   end
 end
 
