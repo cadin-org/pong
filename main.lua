25f0dfeacc887798e53e28c516fa252d2acb32f6
Vinicius Tibo <viniciustibo.vt@gmail.com>
2024-08-14 17:29:34 -0300

    cpu less robotic movements

--- main.lua
+++ main.lua
@@ -67,7 +67,7 @@ function love.update(dt)
   if GAME_STATE == 'playing' then
     if GAME_MODE == 'single_player' then
       paddles[1]:player_move('w', 's')
-      paddles[2]:cpu_move(ball)
+      paddles[2]:cpu_predict_move(ball)
     elseif GAME_MODE == 'multiplayer' then
       paddles[1]:player_move('w', 's')
       paddles[2]:player_move('up', 'down')
@@ -78,9 +78,9 @@ end
 
 function love.draw()
   local time = love.timer.getTime()
-  splash.start(time)
+  -- splash.start(time)
 
-  if time >= 12 then
+  if time >= 0 then
     if GAME_STATE == 'title_screen' then
       menu.draw(MAIN_MENU)
     elseif GAME_STATE == 'pause_screen' then
